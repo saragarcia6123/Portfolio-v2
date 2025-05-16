bb7a0fa7b8991d9bc6290d4bd3117e70356e5bdf
saragarcia6123 <saragarcia6123@gmail.com>
2025-05-16 20:48:15 +0200

    nav & scrollbar tweaks

--- src/components/SectionsNav.tsx
+++ src/components/SectionsNav.tsx
@@ -3,7 +3,7 @@ import { createElement } from "react";
 
 export default function SectionsNav({ selected }: { selected: string }) {
   return (
-    <div className="fixed z-100 top-4 left-1/2 transform -translate-x-1/2 md:hidden flex gap-2 rounded-full justify-center bg-white/70 px-4 py-2">
+    <div className="fixed z-100 top-4 left-1/2 transform -translate-x-1/2 md:hidden flex gap-2 justify-center bg-gray-800/70 px-4 py-2">
       {SECTION_NAMES.map((name: string) => (
         <SectionIcon
           key={`section-tag-${name}`}
@@ -23,8 +23,8 @@ function SectionIcon({ name, selected }: { name: string; selected: boolean }) {
       className="transition-all duration-100 ease-linear"
     >
       <h3
-        className={`px-2 tracking-[.2em] text-xl flex justify-end ${
-          selected ? "text-emerald-600/80" : "text-emerald-950/20"
+        className={`px-2 tracking-[.2em] text-xl flex justify-end hover:text-[var(--secondary)]/75 ${
+          selected ? "text-[var(--secondary)]/75" : "text-[var(--tertiary)]/50"
         } transition-colors duration-200 ease-linear`}
       >
         {SECTIONS.filter((section: Section) => section.name === name).map(
